34da8ddde00b5e5db73c16d610f07a84c2bda3b1
Robin Cernin <rcernin@redhat.com>
2023-03-04 19:21:27 +1000

    Clean Login test

--- test/test_login.py
+++ test/test_login.py
@@ -14,12 +14,11 @@ import pytest as pytest
         }
     ],
 )
-def test_express(actions, credentials):
+def test_login(actions, credentials):
     actions.prepare_tests("test_express")
     actions.username = credentials["username"]
     actions.password = credentials["password"]
 
     actions.login(url="http://127.0.0.1:8000/login", submit="css=.w3-button")
 
-    actions.take_screenshot("login.png")
     actions.go("http://127.0.0.1:8000/logout")
