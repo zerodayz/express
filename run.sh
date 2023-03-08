7c1b2fe7062f2618baea1ea9e9c39ca0505f0b5f
Robin Cernin <rcernin@redhat.com>
2023-03-09 09:40:30 +1000

    Add new run.sh command

---
+++ run.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+WHITE='\033[0;37m'
+PINK='\033[0;35m'
+NC='\033[0m' # No Color
+
+if [ $# -eq 0 ]; then
+    echo -e "${PINK}  _____  __  __  ____${NC}"
+    echo -e "${PINK} | ____| \ \/ / |  _ \\ ${NC}"
+    echo -e "${PINK} |  _|    \  /  | |_) |${NC}"
+    echo -e "${PINK} | |___   /  \  |  __/${NC}"
+    echo -e "${PINK} |_____| /_/\_\ |_|${NC}   The Express Framework for Selenium"
+    echo -e ""
+    echo -e "${WHITE}USAGE:${NC}"
+    echo -e "          run.sh -n|--number <tests_in_parallel> --headless --browser <browser1> --browser <browser2> <test_file.py>"
+    echo -e "${WHITE}EXAMPLES:${NC}"
+    echo -e ""
+    echo -e "   If you are not in a rush, you can run the tests sequentially on a single browser."
+    echo -e ""
+    echo -e "          run.sh --browser chrome test/test_example.py"
+    echo -e ""
+    echo -e "   If you want to run the tests in parallel on a single browser, you can use the -n option."
+    echo -e ""
+    echo -e "          run.sh -n 2 --browser chrome test/test_example.py"
+    echo -e ""
+    echo -e "   If you want to run the tests in parallel on multiple browsers, you can use the --browser option multiple times."
+    echo -e ""
+    echo -e "          run.sh -n 2 --browser chrome --browser firefox test/test_example.py"
+    echo -e ""
+    echo -e "   If you want to run the tests in parallel on multiple browsers in headless mode, you can use the --headless option."
+    echo -e ""
+    echo -e "          run.sh -n 2 --browser chrome --browser firefox --headless test/test_example.py"
+    echo -e ""
+    exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -n|--number)
+            NUMBER="$2"
+            shift
+            shift
+            ;;
+        --browser)
+            BROWSER+=("$2")
+            shift
+            shift
+            ;;
+        --headless)
+            HEADLESS=true
+            shift
+            ;;
+        *)
+            TEST="$1"
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$HEADLESS" ]; then
+    HEADLESS=false
+fi
+
+echo -e "${YELLOW}Running tests...${NC}"
+echo -e "${GREEN}Number of tests in parallel: $NUMBER${NC}"
+echo -e "${GREEN}Headless: $HEADLESS${NC}"
+echo -e "${GREEN}Browsers: ${BROWSER[@]}${NC}"
+echo -e "${GREEN}Test file: $TEST${NC}"
+
+# Construct pytest command
+PYTEST_CMD="python3 -m pytest"
+if [ -n "$NUMBER" ]; then
+    PYTEST_CMD="$PYTEST_CMD -n $NUMBER"
+fi
+if $HEADLESS; then
+    PYTEST_CMD="$PYTEST_CMD --headless"
+fi
+for browser in "${BROWSER[@]}"; do
+    PYTEST_CMD="$PYTEST_CMD --browser=$browser"
+done
+PYTEST_CMD="$PYTEST_CMD $TEST"
+
+echo -e "${YELLOW}Pytest command: $PYTEST_CMD${NC}"
+
+if ! $PYTEST_CMD
+then
+    echo -e "${RED}Failed to run tests.${NC}"
+    exit 1
+fi
--- test/conftest.py
+++ test/conftest.py
@@ -14,13 +14,13 @@ from express import Actions
 
 
 class Express:
-    def __init__(self, driver_name, caplog):
+    def __init__(self, driver_name, headless, caplog):
         self.driver = None
         self.actions = None
 
-        self._init_driver(driver_name, caplog)
+        self._init_driver(driver_name, headless, caplog)
 
-    def _init_driver(self, browser_to_run, caplog):
+    def _init_driver(self, browser_to_run, headless, caplog):
         """Create a driver instance based on the browser to run."""
 
         services = {
@@ -31,7 +31,12 @@ class Express:
             'chrome': webdriver.ChromeOptions(),
             'firefox': webdriver.FirefoxOptions()
         }
-        # options[browser_to_run].add_argument('--headless=new')
+
+        if headless and browser_to_run == 'chrome':
+            options[browser_to_run].add_argument('--headless=new')
+        elif headless and browser_to_run == 'firefox':
+            options[browser_to_run].add_argument('-headless')
+
         if browser_to_run not in services:
             raise Exception(f'Browser {browser_to_run} is not supported.')
 
@@ -45,6 +50,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser', action="append", default=[], choices=['chrome', 'firefox'],
                      help='Your choice of browser '
                           'to run tests.')
+    parser.addoption('--headless', action="store_true", default=False, help='Run tests in headless mode.')
 
 
 def pytest_generate_tests(metafunc):
@@ -55,16 +61,23 @@ def pytest_generate_tests(metafunc):
 
 
 @pytest.fixture(autouse=True)
-def actions(browser_to_run, caplog):
+def actions(request, browser_to_run, caplog):
     """Create an instance of Express class and return it to the test."""
-    x = Express(browser_to_run, caplog)
+    headless = request.config.getoption("headless")
+
+    x = Express(browser_to_run, headless, caplog)
 
     # prepare the test
     full_name = os.environ.get('PYTEST_CURRENT_TEST').split(' ')[0]
     result = re.findall(r"test_\w+", full_name)[0]
-    logging.getLogger().info(f'Running test: {result} on {browser_to_run} browser.')
+    if headless:
+        logging.getLogger().info(f'Running test {result} on {browser_to_run} browser in headless mode.')
+    else:
+        logging.getLogger().info(f'Running test {result} on {browser_to_run} browser.')
     x.actions.prepare_tests(result)
 
     # execute the test
     yield x.actions
+
+    logging.getLogger().info(f'Finished test {result} on {browser_to_run} browser.')
     x.driver.quit()
